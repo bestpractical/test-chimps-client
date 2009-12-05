d48756903e0654ef2fca3ee565add39e778d42d6
Alex Vandiver <alexmv@bestpractical.com>
2009-12-04 22:18:41 -0500

    Revision 0 is possible, and (mostly) valid

--- lib/Test/Chimps/Smoker/SVN.pm
+++ lib/Test/Chimps/Smoker/SVN.pm
@@ -36,7 +36,7 @@ sub committed_date {
 sub clone {
     my $self = shift;
 
-    $self->revision( ($self->revision_info)[0] - 1 ) unless $self->revision;
+    $self->revision( ($self->revision_info)[0] - 1 ) unless defined $self->revision;
     $self->run_cmd("checkout", "-r", $self->revision, $self->uri, $self->directory);
 }
 
